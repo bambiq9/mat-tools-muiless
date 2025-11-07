e2b1f3caf42ee1df2e81160ab18a7bcd48e0d492
Dmitry Egorov <bambiq@yandex.ru>
2025-11-07 23:46:01 +0300

    Updated api to work with json-server

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,11 +1,39 @@
 
 import type {
+	TAssemblyUnit,
+	TAssemblyUnitPart,
+	TAssemblyUnitPartsList,
+	TInsulationGroup,
 	TRegisterData,
 	TUser,
 	TUserCredData,
 } from './types'
 
-export const createNewUserApi = async (data: TUser) => {};
+const API_URL = import.meta.env.VITE_API_URL;
+
+const checkResponse = <T>(res: Response): Promise<T> =>
+  res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
+
+const fetchData = async <T>(path: string, method?: 'POST' | 'PUT', data?: T): Promise<T> => {
+	const options: RequestInit = {
+		method: method || 'GET',
+		headers: {
+      'Content-Type': 'application/json'
+    }
+	}
+
+	if (data) options.body = JSON.stringify(data);
+
+	const res = await fetch(`${API_URL}/${path}`, options);
+	const resData = await checkResponse<T>(res);
+
+	if (resData) return resData;
+	return Promise.reject(resData);
+}
+
+// User
+
+export const createNewUserApi = async (data: TUser) => fetchData('users', 'POST', data);
 
 export const registerUserEmailApi = async (data: TRegisterData) => {}
 
@@ -13,14 +41,20 @@ export const loginUserEmailApi = async (data: TUserCredData) => {}
 
 export const logoutUserApi = async () => {}
 
-export const getUserApi = async (userId: string) => {};
+export const getUserApi = async (userId: string): Promise<TUser> => fetchData(`users/${userId}`);
 
-export const getAssemblyUnitsListApi = async () => {};
+// Assembly units
 
-export const getAssemblyUnitPartsListApi = async () => {};
+export const getAssemblyUnitsListApi = async (): Promise<TAssemblyUnit[]> => fetchData('assemblyUnits');
 
-export const getAssemblyUnitPartApi = async (partId: string) => {}
+export const getAssemblyUnitPartsListApi = async (): Promise<TAssemblyUnitPart[]> => fetchData('assemblyUnitParts');
 
-export const getInsulationItemsApi = async () => {};
+export const getAssemblyUnitPartApi = async (partId: string): Promise<TAssemblyUnitPartsList> => fetchData(`assemblyUnitParts/${partId}`);
 
-export const getInsulationGroupsApi = async () => {};
+// Insulation group
+
+export const getInsulationGroupsApi = async (): Promise<TInsulationGroup[]> => fetchData('insulationGroups');
+
+export const addInsulationGroupApi = async (data: TInsulationGroup): Promise<TInsulationGroup> => fetchData(`insulationGroups`, 'POST', data);
+
+export const updateInsulationGroupApi = async (groupId: string, data: TInsulationGroup): Promise<TInsulationGroup> => fetchData(`insulationGroups/${groupId}`, 'PUT', data);
