9700de80ecfe827f042ee118f963d88ab4f72531
Dmitry Egorov <bambiq@yandex.ru>
2025-11-08 18:01:19 +0300

    refactor: fetchData is now all-purpose function. Updated other api functions

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,26 +1,27 @@
-
 import type {
 	TAssemblyUnit,
 	TAssemblyUnitPart,
 	TAssemblyUnitPartsList,
 	TInsulationGroup,
-	TRegisterData,
 	TUser,
-	TUserCredData,
-} from './types'
+} from './types';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
 const checkResponse = <T>(res: Response): Promise<T> =>
-  res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
+	res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
 
-const fetchData = async <T>(path: string, method?: 'POST' | 'PUT', data?: T): Promise<T> => {
+const fetchData = async <T>(
+	path: string,
+	method: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE' = 'GET',
+	data?: T | Partial<T>
+): Promise<T> => {
 	const options: RequestInit = {
-		method: method || 'GET',
+		method,
 		headers: {
-      'Content-Type': 'application/json'
-    }
-	}
+			'Content-Type': 'application/json',
+		},
+	};
 
 	if (data) options.body = JSON.stringify(data);
 
@@ -29,32 +30,65 @@ const fetchData = async <T>(path: string, method?: 'POST' | 'PUT', data?: T): Pr
 
 	if (resData) return resData;
 	return Promise.reject(resData);
-}
+};
 
 // User
+export const createNewUserApi = async (data: TUser) =>
+	fetchData('users', 'POST', data);
 
-export const createNewUserApi = async (data: TUser) => fetchData('users', 'POST', data);
+// export const registerUserEmailApi = async (data: TRegisterData) => {}
 
-export const registerUserEmailApi = async (data: TRegisterData) => {}
+// export const loginUserEmailApi = async (data: TUserCredData) => {}
 
-export const loginUserEmailApi = async (data: TUserCredData) => {}
+// export const logoutUserApi = async () => {}
 
-export const logoutUserApi = async () => {}
-
-export const getUserApi = async (userId: string): Promise<TUser> => fetchData(`users/${userId}`);
+export const getUserApi = async (userId: string): Promise<TUser> =>
+	fetchData(`users/${userId}`);
 
 // Assembly units
+export const getAssemblyUnitsListApi = async (): Promise<TAssemblyUnit[]> =>
+	fetchData('assemblyUnits');
 
-export const getAssemblyUnitsListApi = async (): Promise<TAssemblyUnit[]> => fetchData('assemblyUnits');
+// Assembly parts list
+export const getAssemblyUnitPartsListApi = async (): Promise<
+	TAssemblyUnitPart[]
+> => fetchData('assemblyUnitParts');
 
-export const getAssemblyUnitPartsListApi = async (): Promise<TAssemblyUnitPart[]> => fetchData('assemblyUnitParts');
+// Assembly part single
+export const getAssemblyUnitPartApi = async (
+	partId: string
+): Promise<TAssemblyUnitPartsList> => fetchData(`assemblyUnitParts/${partId}`);
 
-export const getAssemblyUnitPartApi = async (partId: string): Promise<TAssemblyUnitPartsList> => fetchData(`assemblyUnitParts/${partId}`);
+export const addAssemblyUnitPartApi = async (
+	data: TAssemblyUnitPart
+): Promise<TAssemblyUnitPart> => fetchData('assemblyUnitParts', 'POST', data);
+
+export const updateAssemblyUnitPartApi = async (
+	partId: string,
+	data: Partial<TAssemblyUnitPart>
+): Promise<TAssemblyUnitPart> =>
+	fetchData(`assemblyUnitParts/${partId}`, 'PATCH', data);
+
+export const deleteAssemblyUnitPartApi = async (
+	partId: string
+): Promise<TAssemblyUnitPart> =>
+	fetchData(`assemblyUnitParts/${partId}`, 'DELETE');
 
 // Insulation group
+export const getInsulationGroupsApi = async (): Promise<TInsulationGroup[]> =>
+	fetchData('insulationGroups');
 
-export const getInsulationGroupsApi = async (): Promise<TInsulationGroup[]> => fetchData('insulationGroups');
+export const addInsulationGroupApi = async (
+	data: TInsulationGroup
+): Promise<TInsulationGroup> => fetchData('insulationGroups', 'POST', data);
 
-export const addInsulationGroupApi = async (data: TInsulationGroup): Promise<TInsulationGroup> => fetchData(`insulationGroups`, 'POST', data);
+export const updateInsulationGroupApi = async (
+	groupId: string,
+	data: Partial<TInsulationGroup>
+): Promise<TInsulationGroup> =>
+	fetchData(`insulationGroups/${groupId}`, 'PATCH', data);
 
-export const updateInsulationGroupApi = async (groupId: string, data: TInsulationGroup): Promise<TInsulationGroup> => fetchData(`insulationGroups/${groupId}`, 'PUT', data);
+export const deleteInsulationGroupApi = async (
+	groupId: string
+): Promise<TInsulationGroup> =>
+	fetchData(`insulationGroups/${groupId}`, 'DELETE');
